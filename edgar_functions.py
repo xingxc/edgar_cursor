6b2d1013b37bf39bd8977b8320adbbd59cd20eff
John Xing <johnxing@Apple-Mac-Mini.local>
2023-12-27 21:40:32 -0500

    update ipynb to function with statement scrapping and company_fact scrapping

--- edgar_functions.py
+++ edgar_functions.py
@@ -2,7 +2,9 @@ import pandas as pd
 import requests
 import calendar
 import numpy as np
+import logging
 from bs4 import BeautifulSoup
+
 statement_keys_map = {
     "balance_sheet": [
         "balance sheet",
@@ -322,7 +324,9 @@ def get_statement_soup(ticker, acc_num, statement_name, headers, statement_keys_
     try:
         statement_response = session.get(statement_link, headers=headers)
         statement_response.raise_for_status()  # Check if the request was successful
-        links_logged[f'{ticker}-{statement_name}-{file_name}-{acc_num}'] = statement_link
+        links_logged[
+            f"{ticker}-{statement_name}-{file_name}-{acc_num}"
+        ] = statement_link
 
         if statement_link.endswith(".xml"):
             return BeautifulSoup(
@@ -337,93 +341,132 @@ def get_statement_soup(ticker, acc_num, statement_name, headers, statement_keys_
 
 def extract_columns_values_and_dates_from_statement(soup):
     """
-    Extracts columns, values, and dates from an HTML soup object representing a financial statement.
-
     Args:
-        soup (BeautifulSoup): The BeautifulSoup object of the HTML document.
+        soup (BeautifulSoup): BeautifulSoup object of the statement.
 
     Returns:
-        tuple: Tuple containing columns, values_set, and date_time_index.
+        list: A list of statement column names.
+        list: A list of statement values.
+        pd.DatetimeIndex: A Pandas DatetimeIndex object containing the extracted dates.
+
+    Description:
+        - call get_datetime_index_dates_from_statement to get the date_time_index
+        - find all tables in soup and iterate over them
+        - check table header (th) for unit multiplier and special case scenario
+            - special case scenario: check for values like EPS
+        - search each row of table (tr) for "onclick" attribute
+            - if "onclick" is not found, skip the row
+            - if "onclick" is found
+                - append column title to columns list from "onclick" attribute
+                - for each cell in the row (td)
+                    - find all elements with class "text", "nump", or "num"
+                        - nump: positive values
+                        - num: negative values
+                        - text: skip
+        - return columns, values, and date_time_index
     """
+
     columns = []
     values_set = []
-    date_time_index = get_datetime_index_dates_from_statement(soup)
+
+    date_time_index = get_datetime_index_dates_from_statement(soup=soup)
 
     for table in soup.find_all("table"):
         unit_multiplier = 1
         special_case = False
 
-        # Check table headers for unit multipliers and special cases
+        # Check table header for unit multiplier
         table_header = table.find("th")
         if table_header:
             header_text = table_header.get_text()
-            # Determine unit multiplier based on header text
+
             if "in Thousands" in header_text:
                 unit_multiplier = 1
+
             elif "in Millions" in header_text:
                 unit_multiplier = 1000
+
             # Check for special case scenario
             if "unless otherwise specified" in header_text:
                 special_case = True
 
         # Process each row of the table
-        for row in table.select("tr"):
+        for i, row in enumerate(table.select("tr")):
             onclick_elements = row.select("td.pl a, td.pl.custom a")
             if not onclick_elements:
-                continue
+                continue  # Skip rows without onclick elements
 
-            # Extract column title from 'onclick' attribute
+            # Extract column title from "onclick" attribute
             onclick_attr = onclick_elements[0]["onclick"]
             column_title = onclick_attr.split("defref_")[-1].split("',")[0]
             columns.append(column_title)
 
-            # Initialize values array with NaNs
+            # Inititate values array with NaNs
             values = [np.NaN] * len(date_time_index)
 
             # Process each cell in the row
             for i, cell in enumerate(row.select("td.text, td.nump, td.num")):
                 if "text" in cell.get("class"):
-                    continue
+                    continue  # Skip text cells
 
-                # Clean and parse cell value
-                value = keep_numbers_and_decimals_only_in_string(
+                value = (
                     cell.text.replace("$", "")
                     .replace(",", "")
                     .replace("(", "")
                     .replace(")", "")
                     .strip()
                 )
+
+                # ------ Keep numbers and decimals only in string function ------
+                num = "1234567890."
+
+                def filtering_func(x):
+                    return x in num
+
+                # Filter creates iterator and list converts it back to list.
+                allowed = list(filter(filtering_func, value))
+                value = "".join(allowed)
+                # ------ Keep numbers and decimals only in string function ------
+
                 if value:
                     value = float(value)
-                    # Adjust value based on special case and cell class
+
                     if special_case:
-                        value /= 1000
+                        value = value / 1000
+
                     else:
                         if "nump" in cell.get("class"):
-                            values[i] = value * unit_multiplier
+                            values[i] = (
+                                value * unit_multiplier
+                            )  # converting from unit to thousands
                         else:
                             values[i] = -value * unit_multiplier
 
             values_set.append(values)
-
     return columns, values_set, date_time_index
 
 
 def get_datetime_index_dates_from_statement(soup: BeautifulSoup) -> pd.DatetimeIndex:
     """
-    Extracts datetime index dates from the HTML soup object of a financial statement.
-
     Args:
-        soup (BeautifulSoup): The BeautifulSoup object of the HTML document.
+        soup (BeautifulSoup): BeautifulSoup object of the statement.
 
     Returns:
         pd.DatetimeIndex: A Pandas DatetimeIndex object containing the extracted dates.
+
+    Description:
+        - find all table headers (th) with class "th"
+        - extract the date from the div tag and create list of strings
+        - standardize the date strings
+        - convert the list of strings to a Pandas DatetimeIndex object
+        - return the DatetimeIndex object
     """
     table_headers = soup.find_all("th", {"class": "th"})
     dates = [str(th.div.string) for th in table_headers if th.div and th.div.string]
     dates = [standardize_date(date).replace(".", "") for date in dates]
-    index_dates = pd.to_datetime(dates)
-    return index_dates
+    date_time_index = pd.to_datetime(dates)
+
+    return date_time_index
 
 
 def standardize_date(date: str) -> str:
@@ -435,10 +478,15 @@ def standardize_date(date: str) -> str:
 
     Returns:
         str: The standardized date string.
+
+    Description:
+        - Loops through the abbreviations and and replaces them with the full names
+        - Returns the standardized date string
     """
     for abbr, full in zip(calendar.month_abbr[1:], calendar.month_name[1:]):
         date = date.replace(abbr, full)
     return date
+
 
 def keep_numbers_and_decimals_only_in_string(mixed_string: str):
     """
@@ -453,3 +501,80 @@ def keep_numbers_and_decimals_only_in_string(mixed_string: str):
     num = "1234567890."
     allowed = list(filter(lambda x: x in num, mixed_string))
     return "".join(allowed)
+
+
+def create_dataframe_of_statement_values_columns_dates(
+    values_set, columns, index_dates
+) -> pd.DataFrame:
+    """
+    Args:
+        values_set (list): List of values for each column.
+        columns (list): List of column names.
+        index_dates (pd.DatetimeIndex): DatetimeIndex for the DataFrame index.
+
+    Returns:
+        pd.DataFrame: DataFrame constructed from the given data.
+
+    Description:
+        - Create dataframe from values_set, columns, and index_dates
+        - return dataframe
+    """
+    transposed_values_set = list(zip(*values_set))
+    df = pd.DataFrame(transposed_values_set, columns=columns, index=index_dates)
+    return df
+
+
+def process_one_statement(ticker, acc_num, statement_name, headers):
+    """
+    Args:
+        ticker (str): Ticker of the company.
+        acc_num (str): Accession number of the filing.
+        statement_name (str): Name of the statement.
+        headers (dict): Headers for the request.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the statement data.
+
+
+    Description:
+        - Get the BeautifulSoup object of the statement from get_statement_soup
+        - Extract the columns, values, and date_time_index from extract_columns_values_and_dates_from_statement
+        - Create a DataFrame from create_dataframe_of_statement_values_columns_dates
+        - Transpose the DataFrame and drop duplicates
+        - Return the DataFrame
+
+    """
+
+    try:
+        soup = get_statement_soup(
+            ticker, acc_num, statement_name, headers, statement_keys_map
+        )
+
+    except Exception as e:
+        logging.error(
+            f"Failed to get statement soup: {e} for accession number: {acc_num}"
+        )
+        return None
+
+    if soup:
+        try:
+            (
+                columns,
+                values_set,
+                date_time_index,
+            ) = extract_columns_values_and_dates_from_statement(soup=soup)
+
+            df = create_dataframe_of_statement_values_columns_dates(
+                values_set, columns, date_time_index
+            )
+
+            if not df.empty:
+                df = df.T.drop_duplicates()
+            else:
+                logging.warning(f"Empty DataFrame for accession number: {acc_num}")
+                return None
+
+            return df
+        except Exception as e:
+            logging.error(f"Error processing soup to DataFrame: {e}")
+            return None
